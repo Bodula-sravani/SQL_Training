c4af58dd4a13a1b13fe35bb9a35acdebb7b964b2
Bodula-sravani <sravanibodula@gmail.com>
2023-04-06 12:39:12 +0530

    add execute store procedures

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace SQLConnection
 {
@@ -6,49 +7,101 @@ namespace SQLConnection
     {
         static void Main(string[] args)
         {
-            //string connectionString = "Data Source=DESKTOP-8C83AOT;Initial Catalog=PracticeDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
             string connectionString = "Data Source=DESKTOP-8C83AOT;Initial Catalog=PracticeDatabase;Integrated Security=True;Encrypt=False;";
             SqlConnection connection = new SqlConnection(connectionString);
 
 
             connection.Open();
-            SqlCommand command1 = connection.CreateCommand();
 
-            try
+            using(SqlCommand command = new SqlCommand("fetchdetails", connection))
             {
-                Console.WriteLine("enter id to be deleted in technologies");
-                int Techid = int.Parse(Console.ReadLine().Trim());
-                command1.CommandText = $"select count(*) from technologies where id={Techid}";
-                int count = (int)command1.ExecuteScalar();
-                if (count > 0)
+                // Executing store procedure without parameters
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    command1.CommandText = $"delete from technologies where id={Techid}";
-                    command1.ExecuteReader().Close();
-                    Console.WriteLine("Deleted");
+                    for (int j = 0; j < reader.FieldCount; j++)
+                    {
+                        Console.Write(reader[j] + " ");
+                    }
+                    Console.WriteLine();
                 }
-                else
-                {
-                    Console.WriteLine("Employee doesnt exist");
-                }
+                reader.Close();
             }
-            catch (SqlException e)
-            {
-                Console.WriteLine($"Error: {e.Message}");
 
-            }
-            catch (FormatException e)
+            Console.WriteLine("enter dept id , tech id and locatio to fetch employee details");
+            int d_Id = int.Parse(Console.ReadLine());
+            int t_Id = int.Parse(Console.ReadLine());
+            string location = (Console.ReadLine().Trim());
+            using (SqlCommand command = new SqlCommand("fetch_employees", connection))
             {
-                Console.WriteLine($"Format Error: {e.Message}");
+                // Executing store procedure with parameters
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add("@deptId", SqlDbType.Int).Value = d_Id;
+                command.Parameters.Add("@techId", SqlDbType.Int).Value = t_Id;
+                command.Parameters.Add("@locat", SqlDbType.VarChar).Value = location;
+                 SqlDataReader reader =  command.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int j = 0; j < reader.FieldCount; j++)
+                    {
+                        Console.Write(reader[j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                reader.Close();
             }
-            catch(InvalidOperationException e)
-            {
-                Console.WriteLine($"Error: {e.Message}");
-            }
+
+            //ExecuteNonquery cannot fetch results it can only do update delete but not slelect
+            //ExecuteReader can fetch results
+
+
+
+
+
+
+
+
+            //try
+            //{
+            //    Console.WriteLine("enter id to be deleted in technologies");
+            //    int Techid = int.Parse(Console.ReadLine().Trim());
+            //    command1.CommandText = $"select count(*) from technologies where id={Techid}";
+            //    int count = (int)command1.ExecuteScalar();
+            //    if (count > 0)
+            //    {
+            //        command1.CommandText = $"delete from technologies where id={Techid}";
+            //        command1.ExecuteReader().Close();
+            //        Console.WriteLine("Deleted");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Employee doesnt exist");
+            //    }
+            //}
+            //catch (SqlException e)
+            //{
+            //    Console.WriteLine($"Error: {e.Message}");
+
+            //}
+            //catch (FormatException e)
+            //{
+            //    Console.WriteLine($"Format Error: {e.Message}");
+            //}
+            //catch(InvalidOperationException e)
+            //{
+            //    Console.WriteLine($"Error: {e.Message}");
+            //}
+
+
+
             //Console.WriteLine("Enter Id and name of technology to insert");
             //int TechId = int.Parse( Console.ReadLine() );   
             //string TechName = Console.ReadLine().Trim();
             //command1.CommandText = $"insert into technologies values({TechId},'{TechName}')";
             //command1.ExecuteReader().Close();
+
+
 
             //// To read row by row with all columns
             ////while (reader.Read())
@@ -62,16 +115,19 @@ namespace SQLConnection
             ////}
 
 
+
+
             //Console.WriteLine("Enter technology name to be searchedd");
             //TechName = Console.ReadLine().Trim();
-            command1.CommandText = $"select * from technologies";
-            SqlDataReader reader = command1.ExecuteReader();
-            Console.WriteLine($"Id       Name");
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader.GetInt32(0)}        {reader.GetString(1)}");
-            }
-            reader.Close();
+            //SqlCommand command1 = connection.CreateCommand();
+            //command1.CommandText = $"select * from technologies";
+            //SqlDataReader reader1 = command1.ExecuteReader();
+            //Console.WriteLine($"Id       Name");
+            //while (reader1.Read())
+            //{
+            //    Console.WriteLine($"{reader1.GetInt32(0)}        {reader1.GetString(1)}");
+            //}
+            //reader1.Close();
 
 
 
